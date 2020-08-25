870bbbe4143eca67ab181f06d4218b802b093d00
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-25 15:41:53 +0300

    vue init

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,9 +7,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',
-
+  components: {
+    HelloWorld,
+  },
 });
 </script>
